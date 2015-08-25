f020a7e1056cabc513af2b74a5408308ea524175
pulkit21 <pulkit.ag02@gmail.com>
2015-08-25 18:58:21 +0530

    Removed the request json

--- app/controllers/uber_controller.rb
+++ app/controllers/uber_controller.rb
@@ -1,10 +1,7 @@
 class UberController < ApplicationController
 
   def uber_request
-
-    uber_code = params[:code]
     @uber_response = RequestProcessing.send_params(request, params)
-    render json: @uber_response, status: 200
     user_auth = JSON.parse @uber_response
     auth_token = user_auth["access_token"]
     msg = {
