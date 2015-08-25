2ded3f6ea099f51827f419eb603e28c4a35f314b
pulkit21 <pulkit.ag02@gmail.com>
2015-08-25 19:34:43 +0530

    comit

--- app/controllers/uber_controller.rb
+++ app/controllers/uber_controller.rb
@@ -4,11 +4,11 @@ class UberController < ApplicationController
     @uber_response = RequestProcessing.send_params(request, params)
     user_auth = JSON.parse @uber_response
     auth_token = user_auth["access_token"]
+    render json: "successfull", status: 200
     msg = {
       jid: 'android1@roidsoftware.com',
       auth_token: auth_token
     }
-    render json: "successfull", status: 200
     msg = ActiveSupport::JSON.encode(msg)
     $redis.publish "push.new", msg
   end
