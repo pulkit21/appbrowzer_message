952a2fb7f4eb5ae24f1de882b6a70272e686a293
Venkatesh Rao <venkatesh@baniyafy.com>
2015-08-26 13:50:53 +0530

    take client_jid from params

--- app/controllers/uber_controller.rb
+++ app/controllers/uber_controller.rb
@@ -1,11 +1,12 @@
 class UberController < ApplicationController
 
   def uber_request
+    client_jid = params["client_jid"]
     @uber_response = RequestProcessing.send_params(request, params)
     user_auth = JSON.parse @uber_response
     auth_token = user_auth["access_token"]
     msg = {
-      jid: 'android1@roidsoftware.com',
+      jid: client_jid
       auth_token: auth_token
     }
     render json: "successfull", status: 200
