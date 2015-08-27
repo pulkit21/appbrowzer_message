3895f3f7ebe3c4abef71914649755672ef4854e3
Venkatesh Rao <venkatesh@baniyafy.com>
2015-08-27 13:09:11 +0530

    add card_id to the message json object & change auth_token to access_token

--- app/controllers/uber_controller.rb
+++ app/controllers/uber_controller.rb
@@ -2,13 +2,15 @@ class UberController < ApplicationController
 
   def uber_request
     client_jid = params["client_jid"]
+    card_id = params["card_id"]
     @uber_response = RequestProcessing.send_params(request, params)
     user_auth = JSON.parse @uber_response
-    auth_token = user_auth["access_token"]
+    access_token = user_auth["access_token"]
     render json: "successfull", status: 200
     msg = {
       jid: client_jid,
-      auth_token: auth_token
+      access_token: access_token,
+      card_id: card_id
     }
     msg = ActiveSupport::JSON.encode(msg)
     $redis.publish "push.new", msg
