cacd2ab574ddd32240e4bc973eed724b191c2218
pulkit21 <pulkit.ag02@gmail.com>
2015-09-25 19:27:29 +0530

    Fixed booking process

--- lib/booking_processing.rb
+++ lib/booking_processing.rb
@@ -15,7 +15,7 @@ module BookingProcessing
     request_url = "#{return_uri.protocol}#{return_uri.host}/api/booking_request"
     request = Net::HTTP::Post.new(url)
     request["content-type"] = 'multipart/form-data; boundary=---011000010111000001101001'
-    request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"client_secret\"\r\n\r\n#{booking_client_secret}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"client_id\"\r\n\r\n#{booking_client_id}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"grant_type\"\r\n\r\nauthorization_code\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"redirect_uri\"\r\n\r\n#{request_url}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"code\"\r\n\r\n#{code_params}\r\n-----011000010111000001101001--"
+    request.body = "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"client_secret\"\r\n\r\n6017635e0c82b8c597b469e16330c622ac68b276cff77d74466c0845910df423\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"client_id\"\r\n\r\nbf50f847a8371e710182120cdedf665dbf378254a9ac1bdf637fca0a8570600a\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"grant_type\"\r\n\r\nauthorization_code\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"redirect_uri\"\r\n\r\n#{request_url}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"code\"\r\n\r\n#{code_params}\r\n-----011000010111000001101001--"
     response = http.request(request)
     response.read_body
   end
